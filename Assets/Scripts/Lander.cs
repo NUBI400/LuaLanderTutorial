0c4c9a74e214c2001e8cedf09a41782319988d8d
NUBI <138537612+NUBI400@users.noreply.github.com>
2026-03-14 13:58:24 -0300

    Unity

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -21,11 +22,54 @@ public class Lander : MonoBehaviour {
         {
             float turnSpeed = +100f;
             landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
-        }       
+        }
         if (Keyboard.current.rightArrowKey.isPressed)
         {
             float turnSpeed = -100f;
             landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (!collision2D.gameObject.TryGetComponent(out LandingPad landingPad))
+        {
+            Debug.Log("Crash on terrain");
+            return;
+        } 
+
+
+        float softLandingVelocityMagnitude = 4f;
+        float relativeVelocityMagnitude = collision2D.relativeVelocity.magnitude;
+
+        if (relativeVelocityMagnitude > softLandingVelocityMagnitude)
+        {
+            Debug.Log("Landed too hard!");
+            return;
+        }
+        
+        float dotVector = Vector2.Dot(Vector2.up, transform.up);
+        float minDotVector = .90f;
+        if (dotVector < minDotVector)
+        {
+            Debug.Log("Landed on a too steep angle!");
+            return;
+        }
+
+        Debug.Log("Successful landing!");
+
+        float maxScoreAmountLandingAngle = 100;
+        float scoreDotVectorMultiplier = 10f;
+        float landingAngleScore = maxScoreAmountLandingAngle - math.abs(dotVector - 1f) * scoreDotVectorMultiplier * maxScoreAmountLandingAngle;
+
+        float maxScoreAmountLandingSpeed = 100;
+        float landingSpeedScore = (softLandingVelocityMagnitude - relativeVelocityMagnitude) * maxScoreAmountLandingSpeed;
+
+        Debug.Log("LandingAngleScore: " + landingAngleScore);
+        Debug.Log("LandingSpeedScore: " + landingSpeedScore);
+
+        int score = Mathf.RoundToInt((landingAngleScore + landingSpeedScore) * landingPad.GetScoreMultiplier());
+        Debug.Log("socore: " + score);
+    }
+
 }
